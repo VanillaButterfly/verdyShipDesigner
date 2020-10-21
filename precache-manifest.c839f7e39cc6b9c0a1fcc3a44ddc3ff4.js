3a8ff4d1f2d34fff00bc22e9963996001c3a723d
Verdandi <frederic.heral@laposte.net>
2020-10-21 23:20:41 +0200

    Updates

---
+++ precache-manifest.c839f7e39cc6b9c0a1fcc3a44ddc3ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb83939537d97b2845de16eda2ab82a",
+    "url": "/verdyShipDesigner/index.html"
+  },
+  {
+    "revision": "e388b2ed65d57325614e",
+    "url": "/verdyShipDesigner/static/css/main.4ee23e85.chunk.css"
+  },
+  {
+    "revision": "ac0f91522fcbc37b5ee6",
+    "url": "/verdyShipDesigner/static/js/2.22f60d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/verdyShipDesigner/static/js/2.22f60d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e388b2ed65d57325614e",
+    "url": "/verdyShipDesigner/static/js/main.55ad9071.chunk.js"
+  },
+  {
+    "revision": "bfe550ffcc2520bbd35e",
+    "url": "/verdyShipDesigner/static/js/runtime-main.91dc2313.js"
+  }
+]);
